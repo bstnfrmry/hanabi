c3b1a7c33f02ef2770d7daccff606ead25bbf8ec
Bastien Formery <bastien@hellocomet.co>
2019-08-04 19:15:03 +0200

    Typo

--- src/ai.sample.js
+++ src/ai.sample.js
@@ -1,11 +1,11 @@
 /**
  * This function will be called at each turn with the current player.
- * It has to return the action take by the player.
+ * Returns the action taken by the player.
  * Options:
  * { type: "play", card }
  * { type: "discard", card }
- * { type: "hint-color", color}
- * { type: "hint-value", color }
+ * { type: "hint-color", color }
+ * { type: "hint-value", value }
  */
 function ai({ game, player }) {
   return { type: "discard", card: player.hand[0] }
