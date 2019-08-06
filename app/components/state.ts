5e70b03d8e4a325939cf736e800c7e61849b4d75
Benjamin TD <benjamin.tdm@gmail.com>
2019-08-06 21:04:30 +0200

    discard and play action types

--- app/components/state.ts
+++ app/components/state.ts
@@ -14,28 +14,31 @@ export interface IGameOptions {
   multicolor: boolean;
 }
 
-export enum C {
-  RED = 1,
-  GREEN,
-  BLUE,
-  WHITE,
-  YELLOW,
-  MULTICOLOR
+export enum IColor {
+  RED = "red",
+  GREEN = "green",
+  BLUE = "blue",
+  WHITE = "white",
+  YELLOW = "yellow",
+  MULTICOLOR = "multicolor"
 }
 
-// an array of true (still possible) or false (impossible)
-// e.g. a hint onto an unknown card turns all but one values to false
+export type INumber = 1 | 2 | 3 | 4 | 5;
+
+// an array of 2 (direct hint), 1 (still possible), or 0 (impossible)
+// e.g. a color hint onto an card turns all but one values to 0, and one value to 2.
+// a color hint onto a card give all the other cards in the hand a 0 for that color.
 // it's something public, i.e. information that has been given
 // to all players
 export interface ICardHint {
-  color: boolean[];
-  number: boolean[];
+  color: { [key in IColor]: (0 | 1 | 2) };
+  number: { [key in 1 | 2 | 3 | 4 | 5]: (0 | 1 | 2) };
 }
 
 export type IHand = IHandCard[];
 
 export interface ICard {
-  color: C;
+  color: IColor;
   number: number;
 }
 
@@ -43,12 +46,28 @@ export interface IHandCard extends ICard {
   hint: ICardHint;
 }
 
-export interface IAction {
+export type IActionType = "discard" | "play" | "hint";
+
+export interface IDiscardAction {
+  action: "discard";
+  card: IHandCard;
+  // do we need to know where that card was placed in the hand?
+}
+
+export interface IPlayAction {
+  action: "play";
+  card: IHandCard;
+}
+
+export interface IHintAction {
+  action: "hint";
   from: number;
   to: number;
   type: "color" | "number";
-  value: C | number;
+  value: IColor | INumber;
 }
+
+export type IAction = IDiscardAction | IPlayAction | IHintAction;
 
 export interface IPlayer {
   hand: IHand;
@@ -66,8 +85,11 @@ export interface ITokens {
 export type gameHistory = IGameState[];
 
 export const sampleState: IGameState = {
-  playedCards: [{ color: C.RED, number: 1 }],
-  drawPile: [{ color: C.YELLOW, number: 2 }, { color: C.BLUE, number: 4 }],
+  playedCards: [{ color: IColor.RED, number: 1 }],
+  drawPile: [
+    { color: IColor.YELLOW, number: 2 },
+    { color: IColor.BLUE, number: 4 }
+  ],
   discardPile: [],
   players: [
     {
@@ -75,43 +97,108 @@ export const sampleState: IGameState = {
       id: 0,
       hand: [
         {
-          color: C.MULTICOLOR,
+          color: IColor.MULTICOLOR,
           number: 3,
           hint: {
-            color: [false, false, true, true, true, true],
-            number: [false, false, true, false, false]
+            color: {
+              red: 0,
+              blue: 0,
+              yellow: 0,
+              white: 0,
+              green: 0,
+              multicolor: 2
+            },
+            number: {
+              1: 1,
+              2: 0,
+              3: 1,
+              4: 0,
+              5: 1
+            }
           }
         },
         {
-          color: C.BLUE,
+          color: IColor.BLUE,
           number: 2,
           hint: {
-            color: [true, true, true, true, true, true],
-            number: [true, true, true, true, true, true]
+            color: {
+              red: 1,
+              blue: 1,
+              yellow: 1,
+              white: 1,
+              green: 1,
+              multicolor: 0
+            },
+            number: {
+              1: 0,
+              2: 2,
+              3: 0,
+              4: 0,
+              5: 0
+            }
           }
         },
         {
-          color: C.RED,
+          color: IColor.RED,
           number: 4,
           hint: {
-            color: [true, true, true, true, true, true],
-            number: [true, true, true, true, true, true]
+            color: {
+              red: 1,
+              blue: 1,
+              yellow: 1,
+              white: 1,
+              green: 1,
+              multicolor: 1
+            },
+            number: {
+              1: 1,
+              2: 1,
+              3: 1,
+              4: 1,
+              5: 1
+            }
           }
         },
         {
-          color: C.RED,
+          color: IColor.RED,
           number: 4,
           hint: {
-            color: [true, true, true, true, true, true],
-            number: [true, true, true, true, true, true]
+            color: {
+              red: 1,
+              blue: 1,
+              yellow: 1,
+              white: 1,
+              green: 1,
+              multicolor: 1
+            },
+            number: {
+              1: 1,
+              2: 1,
+              3: 1,
+              4: 1,
+              5: 1
+            }
           }
         },
         {
-          color: C.WHITE,
+          color: IColor.WHITE,
           number: 3,
           hint: {
-            color: [false, false, true, true, true, true],
-            number: [false, false, true, false, false]
+            color: {
+              red: 1,
+              blue: 1,
+              yellow: 1,
+              white: 1,
+              green: 1,
+              multicolor: 1
+            },
+            number: {
+              1: 1,
+              2: 1,
+              3: 1,
+              4: 1,
+              5: 1
+            }
           }
         }
       ]
@@ -121,43 +208,108 @@ export const sampleState: IGameState = {
       id: 1,
       hand: [
         {
-          color: C.BLUE,
+          color: IColor.BLUE,
           number: 1,
           hint: {
-            color: [true, false, true, true, true, true],
-            number: [true, false, true, false, false]
+            color: {
+              red: 1,
+              blue: 1,
+              yellow: 1,
+              white: 0,
+              green: 0,
+              multicolor: 0
+            },
+            number: {
+              1: 1,
+              2: 1,
+              3: 0,
+              4: 0,
+              5: 1
+            }
           }
         },
         {
-          color: C.BLUE,
+          color: IColor.BLUE,
           number: 2,
           hint: {
-            color: [true, true, true, true, true, true],
-            number: [true, true, true, true, true, true]
+            color: {
+              red: 1,
+              blue: 1,
+              yellow: 0,
+              white: 1,
+              green: 1,
+              multicolor: 1
+            },
+            number: {
+              1: 1,
+              2: 1,
+              3: 1,
+              4: 0,
+              5: 1
+            }
           }
         },
         {
-          color: C.WHITE,
+          color: IColor.WHITE,
           number: 1,
           hint: {
-            color: [true, true, true, true, true, true],
-            number: [true, true, true, true, true, true]
+            color: {
+              red: 1,
+              blue: 1,
+              yellow: 1,
+              white: 1,
+              green: 1,
+              multicolor: 0
+            },
+            number: {
+              1: 2,
+              2: 0,
+              3: 0,
+              4: 0,
+              5: 0
+            }
           }
         },
         {
-          color: C.RED,
+          color: IColor.RED,
           number: 2,
           hint: {
-            color: [true, true, true, true, true, true],
-            number: [true, true, true, true, true, true]
+            color: {
+              red: 1,
+              blue: 1,
+              yellow: 1,
+              white: 1,
+              green: 1,
+              multicolor: 1
+            },
+            number: {
+              1: 1,
+              2: 1,
+              3: 1,
+              4: 1,
+              5: 1
+            }
           }
         },
         {
-          color: C.GREEN,
+          color: IColor.GREEN,
           number: 2,
           hint: {
-            color: [false, true, true, true, true, true],
-            number: [false, true, true, false, false]
+            color: {
+              red: 1,
+              blue: 1,
+              yellow: 1,
+              white: 1,
+              green: 1,
+              multicolor: 1
+            },
+            number: {
+              1: 1,
+              2: 1,
+              3: 1,
+              4: 1,
+              5: 1
+            }
           }
         }
       ]
