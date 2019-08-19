69d02e8ce6c0f96b433fcce0b91488312dfd1e59
Bastien Formery <bastien@hellocomet.co>
2019-08-19 05:48:53 +0300

    Offline firebase ✈

--- app/context/database.js
+++ app/context/database.js
@@ -14,13 +14,20 @@ export function useDatabase() {
 export function setupDatabase() {
   if (!firebase.apps.length) {
     firebase.initializeApp({
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID
+      // Local database configuration using firebase-server
+      ...(process.env.FIREBASE_DATABASE_URL && {
+        databaseURL: process.env.FIREBASE_DATABASE_URL
+      }),
+      // Online database configuration
+      ...(process.env.FIREBASE_API_KEY && {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID
+      })
     });
   }
 
