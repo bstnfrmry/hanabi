c2781a23a36d70ea072a969700c4f2ace94ff05a
Benjamin TD <benjamin.tdm@gmail.com>
2019-08-22 18:59:00 +0200

    multicolor cards

--- components/vignettes.jsx
+++ components/vignettes.jsx
@@ -1,34 +1,38 @@
 import React from "react";
 import Vignette from "./vignette";
+import { colors, numbers } from "../game/actions";
 
-export default ({ values, colors, onSelect, pendingHint }) => {
+export default ({ onSelect, pendingHint, multicolorOption }) => {
+  console.log(multicolorOption);
   return (
     <div>
       <div className="flex flex-row">
-        {values.map(value => (
+        {numbers.map(number => (
           <Vignette
             type="number"
-            key={value}
-            value={value}
+            key={number}
+            value={number}
             onClick={onSelect}
             selected={
-              pendingHint.type === "number" && pendingHint.value === value
+              pendingHint.type === "number" && pendingHint.value === number
             }
           />
         ))}
       </div>
       <div className="flex flex-row pt2">
-        {colors.map((color, i) => (
-          <Vignette
-            type="color"
-            key={i}
-            value={color}
-            onClick={onSelect}
-            selected={
-              pendingHint.type === "color" && pendingHint.value === color
-            }
-          />
-        ))}
+        {colors
+          .filter(c => (multicolorOption ? true : c !== "multicolor"))
+          .map((color, i) => (
+            <Vignette
+              type="color"
+              key={i}
+              value={color}
+              onClick={onSelect}
+              selected={
+                pendingHint.type === "color" && pendingHint.value === color
+              }
+            />
+          ))}
       </div>
     </div>
   );
