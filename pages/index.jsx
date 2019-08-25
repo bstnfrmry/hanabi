1f607a72de5e8dd3d7359a1d96f960b6f7ae8eaa
Benjamin TD <benjamin.tdm@gmail.com>
2019-08-25 09:36:12 +0200

    rejoin game

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Button from "../components/button";
 
 export default function Home() {
   const router = useRouter();
+  const [lastGame, setLastGame] = useState(null);
+
+  useEffect(() => {
+    if (localStorage.gameId && localStorage.playerId) {
+      setLastGame({
+        gameId: localStorage.gameId,
+        playerId: localStorage.playerId
+      });
+    }
+  }, []);
 
   return (
-    <div>
-      <Button onClick={() => router.push("/new-game")}>Create a room</Button>
-      <Button onClick={() => router.push("/join-game")}>Join a room</Button>
+    <div className="w-100 h-100 flex flex-column justify-center items-center">
+      <Button className="ma2" onClick={() => router.push("/new-game")}>
+        Create a room
+      </Button>
+      <Button className="ma2" onClick={() => router.push("/join-game")}>
+        Join a room
+      </Button>
+      {lastGame ? (
+        <Button
+          className="ma2"
+          onClick={() =>
+            router.replace({
+              pathname: "/play",
+              query: lastGame
+            })
+          }
+        >
+          Rejoin game
+        </Button>
+      ) : null}
     </div>
   );
 }
