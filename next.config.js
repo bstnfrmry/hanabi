46c84c20a403588a8963b1bf2e24dceda1d0f4cf
Bastien Formery <bastien@hellocomet.co>
2020-02-23 16:08:44 +0100

    Remove offline mode

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
-const withOffline = require("next-offline");
 const withSourceMaps = require("@zeit/next-source-maps");
 
 const config = {
@@ -21,31 +20,7 @@ const config = {
     ];
 
     return config;
-  },
-  transformManifest: manifest => ["/"].concat(manifest), // add the homepage to the cache
-  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-  // turn on the SW in dev mode so that we can actually test it
-  generateInDevMode: false,
-  workboxOpts: {
-    swDest: "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "https-calls",
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }
-    ]
   }
 };
 
-module.exports = withSourceMaps(withOffline(config));
+module.exports = withSourceMaps(config);
