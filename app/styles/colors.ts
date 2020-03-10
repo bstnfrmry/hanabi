7b8a491125ce868032c179e09094bebfc711ce9f
Sarah Mamy <sarah@comet.co>
2020-03-10 23:06:03 +0100

    Add some orphan svg files and fixes a goddam bug

--- app/styles/colors.ts
+++ app/styles/colors.ts
@@ -28,6 +28,19 @@ export const Colors = {
   Multicolor: "#9f7aea" // TODO: conic gradient
 };
 
+const gradient = color => {
+  `<svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="100" height="100" fill="url(#paint0_linear)"/>
+    <defs>
+    <linearGradient id="paint0_linear" x1="0" y1="0" x2="100" y2="100" gradientUnits="userSpaceOnUse">
+    <stop stop-color="${color}"/>
+    <stop offset="0.5" stop-color="${Colors.Gray.Medium}"/>
+    <stop offset="1" stop-color="${color}"/>
+    </linearGradient>
+    </defs>
+  </svg>`;
+};
+
 export const ColorMap = {
   [Color.BLUE]: require("../../assets/images/cards/blue.png"),
   [Color.RED]: require("../../assets/images/cards/red.png"),
