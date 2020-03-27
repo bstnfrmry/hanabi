8beb98acc398865c04f041c831167ece66a9f5ef
Bastien <bastien.formery@gmail.com>
2020-03-27 19:05:05 +0100

    Display a message on error page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -21,6 +21,29 @@ export default class Error extends React.Component<Props> {
       <div className="pa2 pv4-l ph3-l shadow-5 br3 bg-main-dark flex flex-column justify-center items-center w-100 h-100">
         <Txt className="red" size={TxtSize.LARGE} value={statusCode} />
         <Txt className="mb4" value="We're sorry, something went wrong 😕" />
+
+        <Txt className="mv4 tc lh-copy">
+          We'll investigate as soon as possible. Don't hesitate to{" "}
+          <a className="lavender" href="mailto:bastien.formery@gmail.com">
+            reach out
+          </a>{" "}
+          to let us know what happened.
+          <br />
+          The game is still in early development and we'd love your feedback.
+          <br />
+          <br />
+          We also have a{" "}
+          <a
+            className="lavender"
+            href="https://github.com/bstnfrmry/hanabi/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            public Github repository
+          </a>{" "}
+          if you'd like to contribute or report a bug.
+        </Txt>
+
         <Button text="Back to menu" onClick={() => Router.replace("/")} />
       </div>
     );
