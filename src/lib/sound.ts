5377a4bd830b528d26c40f6c16570788b4f3557c
Bastien <bastien.formery@gmail.com>
2020-05-01 11:29:53 +0200

    Never throw when playing sounds

--- src/lib/sound.ts
+++ src/lib/sound.ts
@@ -1,17 +1,5 @@
-export async function playSound(path: string) {
-  new Audio(path).play().catch(e => {
-    // Playing a sound before any user interaction triggers an error
-    // Prevent it from polluting the error logs
-    // @see https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
-    if (e.name === "NotAllowedError") {
-      return;
-    }
-
-    // Some browsers can't play sounds
-    if (e.name === "NotSupportedError") {
-      return;
-    }
-
-    throw e;
+export function playSound(path: string) {
+  new Audio(path).play().catch(error => {
+    console.warn(error);
   });
 }
