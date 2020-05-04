3af6255d537410f4f0663f86e938a95ed38629b2
Bastien <bastien.formery@gmail.com>
2020-05-04 10:14:12 +0200

    Fix default language

--- src/components/languageSelector.tsx
+++ src/components/languageSelector.tsx
@@ -17,7 +17,10 @@ export default function LanguageSelector(props: Props) {
   const { outlined = false } = props;
 
   const { i18n } = useTranslation();
-  const [lang, setLang] = useLocalStorage("lang", i18n.language);
+  // Will be stored as "fr-FR" after automatic language detecion
+  const [defaultLanguage] = i18n.language.split("-");
+
+  const [lang, setLang] = useLocalStorage("lang", defaultLanguage);
 
   useEffect(() => {
     if (!lang) return;
