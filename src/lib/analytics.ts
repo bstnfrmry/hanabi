cedfd11cc361fed0ca37551bea8d18a3546fafba
Bastien <bastien.formery@gmail.com>
2020-05-05 12:51:20 +0200

    Replace FullStory by Hotjar

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -1,4 +1,5 @@
 import ReactGA from "react-ga";
+import { hotjar } from "react-hotjar";
 
 const isDevelopment = process.env.NODE_ENV === "development";
 
@@ -6,6 +7,7 @@ export const initGA = () => {
   if (isDevelopment) return;
 
   ReactGA.initialize("UA-56583082-2");
+  hotjar.initialize(+process.env.HOTJAR_ID, 6);
 };
 
 export const logPageView = () => {
