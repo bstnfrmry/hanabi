f73d97633dfdae2846af3fafa25449e22e1560d8
Bastien <bastien.formery@gmail.com>
2020-05-05 13:00:59 +0200

    Move Google Analaytics ID in env. Disable tracking in preview envs

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -1,13 +1,20 @@
 import ReactGA from "react-ga";
 import { hotjar } from "react-hotjar";
 
-const isDevelopment = process.env.NODE_ENV === "development";
+const { NODE_ENV, GOOGLE_ANALYTICS_ID, HOTJAR_ID } = process.env;
+
+const isDevelopment = NODE_ENV === "development";
 
 export const initGA = () => {
   if (isDevelopment) return;
 
-  ReactGA.initialize("UA-56583082-2");
-  hotjar.initialize(+process.env.HOTJAR_ID, 6);
+  if (GOOGLE_ANALYTICS_ID) {
+    ReactGA.initialize(GOOGLE_ANALYTICS_ID);
+  }
+
+  if (HOTJAR_ID) {
+    hotjar.initialize(+HOTJAR_ID, 6);
+  }
 };
 
 export const logPageView = () => {
