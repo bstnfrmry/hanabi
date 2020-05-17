0b22664a9bc3642ac101a7322e66ff5c86240386
Bastien <bastien.formery@gmail.com>
2020-05-17 16:15:10 +0200

    PWA

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const path = require("path");
 const Dotenv = require("dotenv-webpack");
 const nextSourceMaps = require("@zeit/next-source-maps")();
 const optimizedImages = require("next-optimized-images");
+const nextOffline = require("next-offline");
 const withPlugins = require("next-compose-plugins");
 
 const config = [
@@ -48,4 +49,34 @@ const optimizedImagesConfig = [
   },
 ];
 
-module.exports = withPlugins([config, sourceMapsConfig, optimizedImagesConfig]);
+const offlineConfig = [
+  nextOffline,
+  {
+    transformManifest: manifest => ["/"].concat(manifest), // add the homepage to the cache
+    // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+    // turn on the SW in dev mode so that we can actually test it
+    generateInDevMode: true,
+    workboxOpts: {
+      swDest: "static/service-worker.js",
+      runtimeCaching: [
+        {
+          urlPattern: /^https?.*/,
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "https-calls",
+            networkTimeoutSeconds: 15,
+            expiration: {
+              maxEntries: 150,
+              maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+            },
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+      ],
+    },
+  },
+];
+
+module.exports = withPlugins([config, sourceMapsConfig, offlineConfig, optimizedImagesConfig]);
