ad3c817fc5382784f3c34e097ac061649cf3a44e
Zoltan Szladek <zoltan_szladek@epam.com>
2021-06-21 02:19:03 +0200

    Dangerous card calculation and display fix for critical rainbow

--- src/lib/ai-cheater.ts
+++ src/lib/ai-cheater.ts
@@ -1,5 +1,6 @@
 import { last, sortBy } from "lodash";
 import { commitAction, MaxHints } from "~/lib/actions";
+import { isCriticalCard, isCardDangerous, isCardEverPlayable } from "~/lib/ai";
 import IGameState, { ICard, IColor } from "~/lib/state";
 
 function canPlay(game: IGameState, card: ICard) {
@@ -12,14 +13,12 @@ function canPlay(game: IGameState, card: ICard) {
   return card.number === 1;
 }
 
-const AmountPerColor = { 1: 3, 2: 2, 3: 2, 4: 2, 5: 1 };
-
 function cardIdentity(cardA, cardB) {
   return cardA.color === cardB.color && cardA.number === cardB.number;
 }
 
 function canSafelyDiscard(game: IGameState, card: ICard) {
-  if (card.color === IColor.MULTICOLOR) return false;
+  if (isCriticalCard(card, game) && isCardEverPlayable(card, game)) return false;
 
   if (game.playedCards.find((playedCard) => cardIdentity(card, playedCard))) {
     return true;
@@ -29,15 +28,8 @@ function canSafelyDiscard(game: IGameState, card: ICard) {
 }
 
 function canDiscard(game: IGameState, card: ICard) {
-  if (card.color === IColor.MULTICOLOR) return false;
-
-  if (game.playedCards.find((playedCard) => cardIdentity(card, playedCard))) {
-    return true;
-  }
-
-  const identicalDiscardedCards = game.discardPile.filter((discardedCard) => cardIdentity(card, discardedCard));
-
-  return identicalDiscardedCards.length < AmountPerColor[card.number] - 1;
+  if (isCriticalCard(card, game) && isCardEverPlayable(card, game)) return false;
+  return !isCardDangerous(card, game);
 }
 
 export function cheat(game: IGameState): IGameState {
@@ -47,7 +39,7 @@ export function cheat(game: IGameState): IGameState {
   const playableCards = currentPlayer.hand.filter((card) => canPlay(game, card));
   const [playableCard] = sortBy(
     sortBy(playableCards, (card) => card.number),
-    (card) => card.color === IColor.MULTICOLOR
+    (card) => isCriticalCard(card, game)
   );
   if (playableCard) {
     return commitAction(game, {
