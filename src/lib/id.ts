451759f984cf3410b26b7fa9993e30ef0a2ce5a6
dcorbin <dave@dcorbin.com>
2024-03-23 19:34:38 -0400

    Add an integer to the end of an ID.  Increment that integer when making a new game.

--- src/lib/id.ts
+++ src/lib/id.ts
@@ -4,12 +4,22 @@ import { adjectives, animals, colors, uniqueNamesGenerator } from "unique-names-
 export type ID = string;
 
 export function readableUniqueId(): ID {
-  return uniqueNamesGenerator({
+  const main = uniqueNamesGenerator({
     dictionaries: [adjectives, animals, colors],
     length: 3,
     separator: "",
     style: "capital",
   });
+  return `${main}-0`;
+}
+
+export function nextGameId(existingId: string) {
+  const parts = existingId.split("-");
+  if (parts.length != 2) {
+    throw Error("Invalid ID");
+  }
+  const nextInSequence = parseInt(parts[1]) + 1;
+  return parts[0] + `-${nextInSequence}`;
 }
 
 export function uniqueId(): ID {
